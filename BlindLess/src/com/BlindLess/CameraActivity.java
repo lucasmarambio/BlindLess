de49175d38bc743f578b93aff2ac26a0b899ec78
agustin bravo <harry_agus1@hotmail.com>
2015-08-18 21:51:52 -0300

    Inicio de camera

--- BlindLess/src/com/BlindLess/CameraActivity.java
+++ BlindLess/src/com/BlindLess/CameraActivity.java
@@ -6,115 +6,49 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
+import com.googlecode.tesseract.android.TessBaseAPI;
+
 public class CameraActivity extends Activity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
 	
-	private boolean pictureTaken;
-	private ImageView imagePicture;
-	private String path;
-	public static final int CAMERA_TEXT_REQUEST_CODE = 1;
-	public Thread thread;
-	private ThreadGroup rootThreadGroup = null;
-	private static final String PHOTO_TAKEN	= "photo_taken";
 	
-	@Override
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-//        Thread[] allThreads = null;
-//        
-//        ThreadGroup tg = getRootThreadGroup();
-//        tg.enumerate(allThreads);
-//        if (allThreads != null) {
-//        	for (Thread th : allThreads) {
-//
-//				Log.i("MakeMachine", th.getName() );
-//    			if (th.getName() == "tito") {
-//    				thread = th;
-//    			}
-//    		}
-//		}
-//        MainActivity.tts.sayHello("hola");
-//        ((TextToSpeechActivity) thread).sayHello("Hola");
-        startCameraActivity();
+        setContentView(R.layout.activity_camera);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
     }
 	
-	ThreadGroup getRootThreadGroup( ) {
-	    if ( rootThreadGroup != null )
-	        return rootThreadGroup;
-	    ThreadGroup tg = Thread.currentThread( ).getThreadGroup( );
-	    ThreadGroup ptg;
-	    while ( (ptg = tg.getParent( )) != null )
-	        tg = ptg;
-	    return tg;
+	public static Camera getCameraInstance(){
+	    Camera c = null;
+	    try {
+	        c = Camera.open(); // attempt to get a Camera instance
+	    }
+	    catch (Exception e){
+	        // Camera is not available (in use or does not exist)
+	    }
+	    return c; // returns null if camera is unavailable
 	}
 	
-	protected void startCameraActivity()
-    {
-    	Log.i("MakeMachine", "startCameraActivity()" );
-    	path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/texto.jpg";
-    	File file = new File(path);
-    	Uri outputFileUri = Uri.fromFile( file );
-    	
-    	Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE );
-    	cameraIntent.putExtra( MediaStore.EXTRA_OUTPUT, outputFileUri );
-    	
-    	startActivityForResult( cameraIntent, CAMERA_TEXT_REQUEST_CODE);
-    }    
-  
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) 
-    {	
-    	Log.i( "MakeMachine", "resultCode: " + resultCode ); 	
-    	
-    	if (resultCode == RESULT_OK){
-    		Log.i( "MakeMachine", "User OK" );
-			onPhotoTaken();		
-    	} else if (resultCode == RESULT_CANCELED)
-    	if (resultCode == RESULT_CANCELED){
-    		Log.i( "MakeMachine", "User cancelled");
-    	}
-    	else {
-    		Log.i( "MakeMachine", "nada" );
-    		
-    	}
-    }
- 
-    protected void onPhotoTaken()
-    {
-    	Log.i( "MakeMachine", "onPhotoTaken" );
-    	
-    	pictureTaken = true;
-    	
-    	BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 4;
-    	
-    	Bitmap bitmap = BitmapFactory.decodeFile(path, options );
-    	
-    	imagePicture.setImageBitmap(bitmap);
-  
-//    	field.setVisibility( View.GONE );
-    }
-    
-    @Override 
-    protected void onRestoreInstanceState( Bundle savedInstanceState){
-    	Log.i( "MakeMachine", "onRestoreInstanceState()");
-    	if( savedInstanceState.getBoolean( CameraActivity.PHOTO_TAKEN ) ) {
-    		onPhotoTaken();
-    	}
-    }
-    
-    @Override
-    protected void onSaveInstanceState( Bundle outState ) {
-    	outState.putBoolean( CameraActivity.PHOTO_TAKEN, pictureTaken );
-    }
 	
 
 }
