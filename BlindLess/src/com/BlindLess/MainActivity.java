86fb59afd4bf333d800dcde4cf6237eafdd7ed2a
Rodrigo Ruben Romano <romanorodrigo@ymail.com>
2015-09-12 18:35:33 -0300

    Escaneo de imágenes

--- BlindLess/src/com/BlindLess/MainActivity.java
+++ BlindLess/src/com/BlindLess/MainActivity.java
@@ -17,12 +17,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.BlindLess.R;
+import com.googlecode.tesseract.android.TessBaseAPI;
 
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
+import android.media.ExifInterface;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
@@ -39,9 +44,10 @@ public class MainActivity extends Activity{
 	private TextView field;
 	private Button button;
 	private Button buttonCamera;
+	private Button buttonBillete;
 	private VoiceRecognition voiceRecognition;
     public Speaker speaker; 
-    
+     
     private SpeechRecognizer mSpeechRecognizer;
     private Intent mSpeechRecognizerIntent; 
     private boolean mIslistening; 
@@ -57,6 +63,7 @@ public class MainActivity extends Activity{
 	
 			button = (Button)findViewById(R.id.button1);
 			buttonCamera = (Button)findViewById(R.id.buttonCamera);
+			buttonBillete = (Button)findViewById(R.id.buttonBillete);
 			
 			//Init command dictionary
 			initDictionary();
@@ -67,17 +74,21 @@ public class MainActivity extends Activity{
 		    startActivityForResult(check, TTS_CHECK);
 			
 			buttonCamera.setOnClickListener( new ButtonClickHandler() );
-		
+			//[INICIO].
+			buttonBillete.setOnClickListener( new ButtonClickHandler() );
+			//[FIN].
+			
 			initializeSpeech();
 		    
 		
 		} catch (Exception e) {
 			// TODO: hacer algo
 		}	
-		button.setOnClickListener(new View.OnClickListener() {
-			
+		button.setOnClickListener(new View.OnClickListener() 
+		{	
 			@Override
-			public void onClick(View v) {
+			public void onClick(View v) 
+			{
 				// TODO Auto-generated method stub
 				if (button.getText() == getString(R.string.reconocerVoz))
 				{
@@ -146,13 +157,77 @@ public class MainActivity extends Activity{
     public class ButtonClickHandler implements View.OnClickListener 
     {
     	public void onClick( View view ){
-    		switch (view.getId()) {
+    		//[INICIO]    		
+    	    String path_ocr;
+    	    //[FIN]
+    		
+    	    switch (view.getId()) {
 			case R.id.buttonCamera:
 //				SingletonTextToSpeech.getInstance(getApplicationContext()).sayHello("Iniciando Cámara");
 				speaker.speak("Iniciando cámara");
 		    	Intent intent = new Intent(getApplicationContext(), CameraActivity.class );
 		    	startActivity(intent);
 				break;
+//[INICIO] Comenzando con las pruebas para detectar texto.
+			case R.id.buttonBillete:
+				path_ocr = "/storage/sdcard0/Pictures/BlindLess Pics/BlindLess6.jpg";
+				ExifInterface exif;
+				try {
+					exif = new ExifInterface(path_ocr);
+					int exifOrientation = exif.getAttributeInt(
+					        ExifInterface.TAG_ORIENTATION,
+					        ExifInterface.ORIENTATION_NORMAL);
+
+					int rotate = 0;
+
+					switch (exifOrientation) {
+					case ExifInterface.ORIENTATION_ROTATE_90:
+					    rotate = 90;
+					    break;
+					case ExifInterface.ORIENTATION_ROTATE_180:
+					    rotate = 180;
+					    break;
+					case ExifInterface.ORIENTATION_ROTATE_270:
+					    rotate = 270;
+					    break;
+					default:
+						break;
+					}
+					
+					BitmapFactory.Options options = new BitmapFactory.Options();
+				    options.inSampleSize = 4;
+				    	
+				    Bitmap bitmap = BitmapFactory.decodeFile( path_ocr, options );
+				    //_image.setImageBitmap(bitmap);
+					
+					if (rotate != 0) {
+					    int w = bitmap.getWidth();
+					    int h = bitmap.getHeight();
+
+					    // Setting pre rotate
+					    Matrix mtx = new Matrix();
+					    mtx.preRotate(rotate);
+
+					    // Rotating Bitmap & convert to ARGB_8888, required by tess
+					    bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+					}
+					bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+					TessBaseAPI baseApi = new TessBaseAPI();
+					// DATA_PATH = Path to the storage
+					// lang = for which the language data exists, usually "eng"
+					baseApi.init("/storage/sdcard0/", "eng");
+					//baseApi.init("/storage/sdcard0/tessdata/spa.traineddata", "spa");
+					// Eg. baseApi.init("/mnt/sdcard/tesseract/tessdata/eng.traineddata", "eng");
+					baseApi.setImage(bitmap);
+					String recognizedText = baseApi.getUTF8Text();
+					baseApi.end();
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+												
+//[FIN] Comenzando con las pruebas para detectar texto.
 			default:
 				break;
 			}
