5179410e6ce92f9525f68ea1acaf30073a9b2d4b
Lucas <lucas.e.marambio@gmail.com>
2015-09-26 18:19:11 -0300

    - Ahora la cámara se ve bien, ya no se ve más estirada como estaba antes! :) - Ahora se puede inicializar la cámara pasando un callback para decir que hacer con la imagen que se tomó. De esta manera para los billetes y para los textos podeos hacer cosas distintas. - Borre algunos metodos que eran inutiles. - Ahora apenas inicializa la cámara saca foto automatica.

--- BlindLess/src/com/BlindLess/Command.java
+++ BlindLess/src/com/BlindLess/Command.java
@@ -1,6 +1,12 @@
 package com.BlindLess;
 
+import android.hardware.Camera;
+
 
 interface Command {
     void runCommand();
+}
+
+interface CommandCamera {
+	void runCommand(byte[] data, Camera camera);
 }
