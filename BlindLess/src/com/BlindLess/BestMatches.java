352e9eca34839817887002b5dede862fc3799a6f
Lucas <lucas.e.marambio@gmail.com>
2015-10-26 20:09:22 -0300

    - Otra clase

---
+++ BlindLess/src/com/BlindLess/BestMatches.java
@@ -0,0 +1,30 @@
+package com.BlindLess;
+
+import android.graphics.Bitmap;
+
+public class BestMatches {
+
+	private Bitmap image;
+	private double maxVal;
+
+	public BestMatches(Bitmap image, double maxVal) {
+		this.image = image;
+		this.maxVal = maxVal;
+	}
+
+	public Bitmap getImage() {
+		return image;
+	}
+
+	public void setImage(Bitmap image) {
+		this.image = image;
+	}
+
+	public double getMaxVal() {
+		return maxVal;
+	}
+
+	public void setMaxVal(double maxVal) {
+		this.maxVal = maxVal;
+	}
+}
